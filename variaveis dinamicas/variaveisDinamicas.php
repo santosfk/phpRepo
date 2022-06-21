b3487618271822840efa81a7a8114fdf440c0fa4
santosfk <santosguilhermefk@gmail.com>
2022-06-21 13:27:50 -0300

    adicionando conteudo sobre variaveis dinamicas

---
+++ variaveis dinamicas/variaveisDinamicas.php
@@ -0,0 +1,12 @@
+<?php
+
+$veiculo = "carro";
+
+$$veiculo = "jaguar";
+
+echo $carro;
+
+// ele converte o valor recebido em veiculo em uma variavel que recebe o valor "jaguar", 
+// em outros termos é uma variavel de uma variavel
+
+?>
