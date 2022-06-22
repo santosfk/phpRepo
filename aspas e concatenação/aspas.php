f7c75165100c324bc50de6c4e8f7898dc19b3b17
santosfk <santosguilhermefk@gmail.com>
2022-06-21 23:15:51 -0300

    adicionando conteudo sobre aspas

---
+++ aspas e concatenação/aspas.php
@@ -0,0 +1,10 @@
+<?php
+//aspas simples são literais
+$nome = "Rodrigo de oliveira";
+echo 'meu nome é: $nome |';
+echo 'meu nome é:'.$nome;
+
+// aspas duplas são interpretativas
+$idade= 22;
+echo "|minha idade é de $idade anos";
+?>
