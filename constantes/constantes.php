3df9188e75b3632d044318693922a7d954594931
santosfk <santosguilhermefk@gmail.com>
2022-06-22 00:17:51 -0300

    conteudo sobre constantes

---
+++ constantes/constantes.php
@@ -0,0 +1,10 @@
+<?php
+define ("NOME","guizo");
+define ("IDADE",26);
+define ("ADULTO",true);
+
+var_dump(NOME);
+var_dump(IDADE);
+var_dump(ADULTO);
+echo 'meu nome é'.NOME."e minha idade é".IDADE;
+?>
