4e38fe84971f06b75394fb19b5f59f1013ca5254
santosfk <santosguilhermefk@gmail.com>
2022-06-22 08:43:49 -0300

    conteudo de introdução aos arrays

---
+++ arrays/introduçãoArrays.php
@@ -0,0 +1,22 @@
+<?php
+$carros = array("fusca","chevette","brasilia");
+print_r($carros);
+echo $carros[0];
+
+
+$carros[]= "gol"; //adicionando item ao array
+print_r($carros);
+
+
+$carros[10]="palio"; //especificando posição do item
+print_r($carros);
+
+$motos = array();
+$motos[]= "yamaha";
+$motos[]="honda";
+print_r($motos);
+
+
+$perifericos = ["mouse","teclado","monitor"];
+print_r($perifericos);
+?>
