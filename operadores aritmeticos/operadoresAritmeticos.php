23acaa75d70717b2cd5675bbc323291ba4a81762
santosfk <santosguilhermefk@gmail.com>
2022-06-23 15:14:24 -0300

    conteudo sobre operadores aritmeticos

---
+++ operadores aritmeticos/operadoresAritmeticos.php
@@ -0,0 +1,31 @@
+<?php
+echo 5+5;//adição
+echo "|";
+echo 5*5; // multiplicação
+echo "|";
+echo 5-5; // subtração
+echo "|";
+echo 5/5; // divisão
+echo "|";
+echo 5%5; //módulo
+echo "|";
+echo 5**5; //exponenciação
+
+echo "<br>";
+
+$numero1=4;
+$numero2=4;
+
+echo $numero1+$numero2;//adição
+echo "|";
+echo $numero1*$numero2; // multiplicação
+echo "|";
+echo $numero1-$numero2; // subtração
+echo "|";
+echo $numero1/$numero2; // divisão
+echo "|";
+echo $numero1%$numero2; //módulo
+echo "|";
+echo $numero1**$numero2; //exponenciação
+
+?>
